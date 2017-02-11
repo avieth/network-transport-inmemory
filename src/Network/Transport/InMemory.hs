c6893b17531ed47838c8d8f0bf434cf0119b50df
Alexander Vieth <aovieth@gmail.com>
2017-02-11 13:27:36 -0500

    Implement ConnectionBundle
    See network-transport commit e7a5f44d0d98370d16df103c9dc61ef7bf15aee8

--- src/Network/Transport/InMemory.hs
+++ src/Network/Transport/InMemory.hs
@@ -12,12 +12,15 @@ module Network.Transport.InMemory
   , breakConnection
   ) where
 
+import Control.Monad (forM)
 import Control.Applicative
 import Network.Transport
 import Network.Transport.Internal ( mapIOException )
 import Control.Category ((>>>))
 import Control.Concurrent.STM
 import Control.Exception (handle, throw)
+import Data.Word (Word32, Word64)
+import Data.Bits (shiftL, shiftR, (.|.))
 import Data.Map (Map)
 import Data.Maybe (fromJust)
 import Data.Monoid
@@ -52,9 +55,19 @@ data LocalEndPointState
   | LocalEndPointClosed
 
 data ValidLocalEndPointState = ValidLocalEndPointState
-  { _nextConnectionId :: !ConnectionId
-  , _connections :: !(Map (EndPointAddress,ConnectionId) LocalConnection)
+  { _nextHeavyweightConnectionId :: !HeavyweightConnectionId
+  , _nextLightweightConnectionId :: !LightweightConnectionId
+    -- For each EndPointAddress (a peer) there can be only one
+    -- HeavyweightConnectionId at any given time.
+  , _connections :: !(Map EndPointAddress LocalConnections)
   , _multigroups :: Map MulticastAddress (TVar (Set EndPointAddress))
+  }
+
+-- | Connections to another EndPoint. Connector chooses the
+--   HeavyweightConnectionId, connected chooses the LightweightConnectionId.
+data LocalConnections = LocalConnections
+  { localConnectionsHeavyweightConnectionId :: !HeavyweightConnectionId
+  , localConnections :: !(Map LightweightConnectionId LocalConnection)
   }
 
 data LocalConnection = LocalConnection
@@ -68,6 +81,57 @@ data LocalConnectionState
   = LocalConnectionValid
   | LocalConnectionClosed
   | LocalConnectionFailed
+
+-- | A 'ConnectionId' is the concatenation of a
+--   'HeavyweightConnectionId' and a 'LightweightConnectionId'.
+--   See 'createConnectionId'.
+type LightweightConnectionId = Word32
+
+firstLightweightConnectionId :: LightweightConnectionId
+firstLightweightConnectionId = 0
+
+-- | A 'ConnectionId' is the concatenation of a
+--   'HeavyweightConnectionId' and a 'LightweightConnectionId'.
+--   See 'createConnectionId'.
+type HeavyweightConnectionId = Word32
+
+-- [Note: computing the ConnectionId]
+--
+-- Suppose A connects to B. From these two EndPoints we must come up with
+-- a ConnectionId which is unique in B, and also a HeavyweightConnectionId
+-- which is unique in A (so that it can be given to the Connection constructor).
+-- The solution: source the LightweightConnectionId from B (it knows the next
+-- one to use) and the HeavyweightConnectionId from A (similarly, it knows the
+-- next one to use).
+
+firstHeavyweightConnectionId :: HeavyweightConnectionId
+firstHeavyweightConnectionId = 0
+
+-- | Use a 'LightweightConnectionId' and a 'HeavyweightConnectionId' to build
+--   a network-transport 'ConnectionId'.
+createConnectionId
+  :: HeavyweightConnectionId
+  -> LightweightConnectionId
+  -> ConnectionId
+createConnectionId hcid lcid =
+  (makeWord64 hcid `shiftL` 32) .|. makeWord64 lcid
+  where
+  makeWord64 :: Word32 -> Word64
+  makeWord64 = fromIntegral
+
+-- | Decompose a 64-bit `ConnectionId` into its 32-bit heavyweight and
+--   lightweight identifier components.
+--
+--     uncurry createConnectionId . deconstructConnectionId = id
+--
+deconstructConnectionId
+  :: ConnectionId
+  -> (HeavyweightConnectionId, LightweightConnectionId)
+deconstructConnectionId connid =
+  (makeWord32 (connid `shiftR` 32), makeWord32 connid)
+  where
+  makeWord32 :: Word64 -> Word32
+  makeWord32 = fromIntegral
 
 newtype TransportInternals = TransportInternals (TVar TransportState)
 
@@ -100,7 +164,10 @@ createTransportExposeInternals = do
               cons <- atomically $ whenValidLocalEndPointState l $ \lvst -> do
                 writeTChan (localEndPointChannel l) EndPointClosed
                 writeTVar  (localEndPointState l) LocalEndPointClosed
-                return (lvst ^. connections)
+                return $ do
+                  (_, lconns) <- Map.toList (lvst ^. connections)
+                  (_, lconn) <- Map.toList (localConnections lconns)
+                  return lconn
               forM_ cons $ \con -> atomically $
                 writeTVar (localConnectionState con) LocalConnectionClosed
     }, TransportInternals state)
@@ -112,7 +179,8 @@ apiNewEndPoint state = handle (return . Left) $ atomically $ do
   chan <- newTChan
   (lep,addr) <- withValidTransportState state NewEndPointFailed $ \vst -> do
     lepState <- newTVar $ LocalEndPointValid $ ValidLocalEndPointState
-      { _nextConnectionId = 1
+      { _nextHeavyweightConnectionId = firstHeavyweightConnectionId
+      , _nextLightweightConnectionId = firstLightweightConnectionId
       , _connections = Map.empty
       , _multigroups = Map.empty
       }
@@ -155,7 +223,11 @@ apiCloseEndPoint state addr = atomically $ whenValidTransportState state $ \vst 
       case old of
         LocalEndPointClosed -> return ()
         LocalEndPointValid lepvst -> do
-          forM_ (Map.elems (lepvst ^. connections)) $ \lconn -> do
+          let lst = do
+                (addr, lconns) <- Map.toList (lepvst ^. connections)
+                (lwcid, lconn) <- Map.toList (localConnections lconns)
+                return lconn
+          forM_ lst $ \lconn -> do
             st <- swapTVar (localConnectionState lconn) LocalConnectionClosed
             case st of
               LocalConnectionClosed -> return ()
@@ -165,7 +237,7 @@ apiCloseEndPoint state addr = atomically $ whenValidTransportState state $ \vst 
                         writeTChan (localEndPointChannel thep)
                                    (ConnectionClosed (localConnectionId lconn))
           writeTChan (localEndPointChannel lep) EndPointClosed
-          writeTVar  (localEndPointState lep)    LocalEndPointClosed
+          writeTVar  (localEndPointState lep)   LocalEndPointClosed
       writeTVar state (TransportValid $ (localEndPoints ^: Map.delete addr) vst)
 
 -- | Function that simulate failing connection between two endpoints,
@@ -195,17 +267,23 @@ apiBreakConnection state us them msg
     breakOne vst a b = do
       forM_ (vst ^. localEndPointAt a) $ \lep ->
         whenValidLocalEndPointState lep $ \lepvst -> do
-          let (cl, other) = Map.partitionWithKey (\(addr,_) _ -> addr == b)
-                                                 (lepvst ^.connections)
-          forM_ cl $ \c -> modifyTVar (localConnectionState c)
-                                      (\x -> case x of
-                                               LocalConnectionValid -> LocalConnectionFailed
-                                               _ -> x)
-          writeTChan (localEndPointChannel lep)
-                     (ErrorEvent (TransportError (EventConnectionLost b) msg))
-          writeTVar (localEndPointState lep)
-                    (LocalEndPointValid $ (connections ^= other) lepvst)
-
+          let mlconns = Map.lookup b (lepvst ^. connections)
+          case mlconns of
+            Nothing -> return ()
+            Just lconns -> do
+              let hwcid = localConnectionsHeavyweightConnectionId lconns
+              let connList = Map.toList (localConnections lconns)
+              lcids <- forM connList $ \(lcid, conn) -> do
+                modifyTVar (localConnectionState conn)
+                           (\x -> case x of
+                                    LocalConnectionValid -> LocalConnectionFailed
+                                    _ -> x)
+                return lcid
+              writeTChan (localEndPointChannel lep)
+                         (ErrorEvent (TransportError (EventConnectionLost b hwcid) msg))
+              let connections' = Map.delete b (lepvst ^. connections)
+              writeTVar (localEndPointState lep)
+                        (LocalEndPointValid $ (connections ^= connections') lepvst)
 
 -- | Create a new connection
 apiConnect :: EndPointAddress
@@ -216,8 +294,10 @@ apiConnect :: EndPointAddress
            -> IO (Either (TransportError ConnectErrorCode) Connection)
 apiConnect ourAddress state theirAddress _reliability _hints = do
     handle (return . Left) $ fmap Right $ atomically $ do
-      (chan, lconn) <- do
+      (chan, lconn, hwcid, lwcid) <- do
         withValidTransportState state ConnectFailed $ \vst -> do
+          -- First, look up the local and remote 'EndPoint's in the transport
+          -- state, failing if either is not there.
           ourlep <- case vst ^. localEndPointAt ourAddress of
                       Nothing ->
                         throwSTM $ TransportError ConnectFailed "Endpoint closed"
@@ -226,27 +306,48 @@ apiConnect ourAddress state theirAddress _reliability _hints = do
                         Nothing ->
                           throwSTM $ TransportError ConnectNotFound "Endpoint not found"
                         Just x  -> return x
-          conid <- withValidLocalEndPointState theirlep ConnectFailed $ \lepvst -> do
-            let r = nextConnectionId ^: (+ 1) $ lepvst
+          -- Increment and return the LightweightConnectionId for the remote
+          -- one.
+          lwcid <- withValidLocalEndPointState theirlep ConnectFailed $ \lepvst -> do
+            let r = nextLightweightConnectionId ^: (+ 1) $ lepvst
             writeTVar (localEndPointState theirlep) (LocalEndPointValid r)
-            return (r ^. nextConnectionId)
+            return (r ^. nextLightweightConnectionId)
+          -- There may already be a connection. In that case, re-use the
+          -- 'HeavyweightConnectionId'. Otherwise, use the next one.
           withValidLocalEndPointState ourlep ConnectFailed $ \lepvst -> do
+            (hwcid, connections') <- case Map.lookup theirAddress (lepvst ^. connections) of
+              -- This is the first connection to that peer.
+              Nothing -> do
+                let r = nextHeavyweightConnectionId ^: (+ 1) $ lepvst
+                writeTVar (localEndPointState ourlep) (LocalEndPointValid r)
+                let hwcid = r ^. nextHeavyweightConnectionId
+                let connections' = \lconn -> Map.insert theirAddress (LocalConnections hwcid (Map.singleton lwcid lconn)) (lepvst ^. connections)
+                return (hwcid, connections')
+              -- We already have a connection to this peer.
+              Just lconns -> do
+                let hwcid = localConnectionsHeavyweightConnectionId lconns
+                let adjuster lconn lconns = lconns {
+                        localConnections = Map.insert lwcid lconn (localConnections lconns)
+                      }
+                let connections' = \lconn -> Map.adjust (adjuster lconn) theirAddress (lepvst ^. connections)
+                return (hwcid, connections')
+            let connid = createConnectionId hwcid lwcid
             lconnState <- newTVar LocalConnectionValid
             let lconn = LocalConnection
-                           { localConnectionId = conid
+                           { localConnectionId = connid
                            , localConnectionLocalAddress = ourAddress
                            , localConnectionRemoteAddress = theirAddress
                            , localConnectionState = lconnState
                            }
             writeTVar (localEndPointState ourlep)
-                      (LocalEndPointValid $
-                         connectionAt (theirAddress, conid) ^= lconn $ lepvst)
-            return (localEndPointChannel theirlep, lconn)
+                      (LocalEndPointValid $ connections ^= (connections' lconn) $ lepvst)
+            return (localEndPointChannel theirlep, lconn, hwcid, lwcid)
       writeTChan chan $
         ConnectionOpened (localConnectionId lconn) ReliableOrdered ourAddress
       return $ Connection
         { send  = apiSend chan state lconn
         , close = apiClose chan state lconn
+        , bundle = hwcid
         }
 
 -- | Send a message over a connection
@@ -303,12 +404,22 @@ apiClose chan state lconn = do
         whenValidTransportState state $ \vst -> do
           let mblep = vst ^. localEndPointAt (localConnectionLocalAddress lconn)
               theirAddress = localConnectionRemoteAddress lconn
+          let updater :: LocalConnections -> Maybe LocalConnections
+              updater lconns =
+                let localConnections' = Map.delete lcid (localConnections lconns)
+                in  if Map.null localConnections'
+                    then Nothing
+                    else Just $ lconns {
+                             localConnections = localConnections'
+                           }
           forM_ mblep $ \lep ->
             whenValidLocalEndPointState lep $
               writeTVar (localEndPointState lep)
                 . LocalEndPointValid
-                . (connections ^: Map.delete (theirAddress, localConnectionId lconn))
+                . (connections ^: Map.update updater theirAddress)
       _ -> return ()
+  where
+  (_, lcid) = deconstructConnectionId (localConnectionId lconn)
 
 -- [Multicast]
 -- Currently multicast implementation doesn't pass it's tests, so it
@@ -396,10 +507,13 @@ nextLocalEndPointId = accessor _nextLocalEndPointId (\eid st -> st{ _nextLocalEn
 localEndPoints :: Accessor ValidTransportState (Map EndPointAddress LocalEndPoint)
 localEndPoints = accessor _localEndPoints (\leps st -> st { _localEndPoints = leps })
 
-nextConnectionId :: Accessor ValidLocalEndPointState ConnectionId
-nextConnectionId = accessor _nextConnectionId (\cid st -> st { _nextConnectionId = cid })
+nextLightweightConnectionId :: Accessor ValidLocalEndPointState LightweightConnectionId
+nextLightweightConnectionId = accessor _nextLightweightConnectionId (\lwcid st -> st { _nextLightweightConnectionId = lwcid })
 
-connections :: Accessor ValidLocalEndPointState (Map (EndPointAddress,ConnectionId) LocalConnection)
+nextHeavyweightConnectionId :: Accessor ValidLocalEndPointState HeavyweightConnectionId
+nextHeavyweightConnectionId = accessor _nextHeavyweightConnectionId (\hwcid st -> st { _nextHeavyweightConnectionId = hwcid })
+
+connections :: Accessor ValidLocalEndPointState (Map EndPointAddress LocalConnections)
 connections = accessor _connections (\conns st -> st { _connections = conns })
 
 multigroups :: Accessor ValidLocalEndPointState (Map MulticastAddress (TVar (Set EndPointAddress)))
@@ -410,9 +524,6 @@ at k err = accessor (Map.findWithDefault (error err) k) (Map.insert k)
 
 localEndPointAt :: EndPointAddress -> Accessor ValidTransportState (Maybe LocalEndPoint)
 localEndPointAt addr = localEndPoints >>> DAC.mapMaybe addr
-
-connectionAt :: (EndPointAddress, ConnectionId) -> Accessor ValidLocalEndPointState LocalConnection
-connectionAt addr = connections >>> at addr "Invalid connection"
 
 multigroupAt :: MulticastAddress -> Accessor ValidLocalEndPointState (TVar (Set EndPointAddress))
 multigroupAt addr = multigroups >>> at addr "Invalid multigroup"
